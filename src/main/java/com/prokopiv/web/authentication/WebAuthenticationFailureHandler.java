59a50dfb81bae9bcab09d3b2d157cd1e5aeda8b2
California <California@188.247.101.145>
2015-03-07 15:00:46 +0200

    piu piu
    Change-Id: I5e0bad28c2ca313360a6bcb2257e5735b738c83b

--- src/main/java/com/prokopiv/web/authentication/WebAuthenticationFailureHandler.java
+++ src/main/java/com/prokopiv/web/authentication/WebAuthenticationFailureHandler.java
@@ -5,19 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.WebAttributes;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.util.UrlUtils;
-import org.springframework.util.Assert;
 
 public class WebAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler  {
 
@@ -30,11 +21,9 @@ public class WebAuthenticationFailureHandler extends SimpleUrlAuthenticationFail
 			logger.error("Нет подключения к Базе данных. Проверьте подключения к БД. (dbconfig.preperties)", exception);
 			request.getSession(true).setAttribute("connect", false);
 			setDefaultFailureUrl("/login");
-//			response.sendRedirect("login");
 		} 
 		setDefaultFailureUrl("/loginError");
 		super.onAuthenticationFailure(request, response, exception);
-		
 	}    
 	
 }
